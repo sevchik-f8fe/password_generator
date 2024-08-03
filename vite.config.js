ded5669f848ef67cf851d64db185d98e9816887c
sevchik-f8fe <kononovseva0627@gmail.com>
2024-08-03 15:51:18 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import vitePluginFaviconsInject from 'vite-plugin-favicons-inject'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // vitePluginFaviconsInject('./src/assets/favicon.ico'),
+  ],
   base: '/password_generator',
 })
